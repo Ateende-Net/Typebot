23ae4f481aaeed66694abc691b2e03744cd98e8b
Paulo Rogério Martins Marques <paulo.analista@outlook.com>
2024-03-16 09:56:45 -0300

    Ajustes no plano

--- apps/builder/src/features/workspace/components/WorkspaceDropdown.tsx
+++ apps/builder/src/features/workspace/components/WorkspaceDropdown.tsx
@@ -38,7 +38,7 @@ export const WorkspaceDropdown = ({
   const workspaces = data?.workspaces ?? []
 
   return (
-    {workspace.plan == "UNLIMITED"?
+    {currentWorkspace.plan == 'UNLIMITED' ?
     <Menu placement="bottom-end">
       <MenuButton as={Button} variant="outline" px="2">
         <HStack>
@@ -84,7 +84,8 @@ export const WorkspaceDropdown = ({
         </MenuItem>
       </MenuList>
     </Menu>
-    : </View>
+    : 
+    </View>
     }
   )
 }
